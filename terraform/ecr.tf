fbf0e9c6637a5cb2e4ed3dd493c5cf8fe4bf5979
Max Ryan Synnott <maxryansynnott@gmail.com>
2021-11-06 09:22:59 +0800

    Added Dockerfile.migrator with ci push image to ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,8 +2,35 @@ resource "aws_ecr_repository" "botletics_server" {
   name = "botletics-server-production"
 }
 
-resource "aws_ecr_lifecycle_policy" "ecr_repo_lifecycle_policy" {
+resource "aws_ecr_lifecycle_policy" "server" {
   repository = aws_ecr_repository.botletics_server.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Only keep one untagged image",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_repository" "botletics_migrator" {
+  name = "botletics-db-migrator"
+}
+
+resource "aws_ecr_lifecycle_policy" "migrator" {
+  repository = aws_ecr_repository.botletics_migrator.name
 
   policy = <<EOF
 {
