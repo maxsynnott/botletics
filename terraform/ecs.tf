8c74b0cc20793ef9ef428c1465c67cec02c6fa1d
Max Ryan Synnott <maxryansynnott@gmail.com>
2021-11-06 09:57:03 +0800

    Added migrator ecs task defintion

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,13 +2,13 @@ resource "aws_ecs_cluster" "botletics" {
   name = "botletics"
 }
 
-resource "aws_ecs_task_definition" "botletics" {
-  family                   = "botletics"
+resource "aws_ecs_task_definition" "botletics_server" {
+  family                   = "botletics-server"
   network_mode             = "awsvpc"
   requires_compatibilities = ["EC2"]
   container_definitions = jsonencode([
     {
-      name      = "botletics-1"
+      name      = "botletics-server-1"
       image     = "${aws_ecr_repository.botletics_server.repository_url}:latest"
       essential = true
       portMappings = [
@@ -32,10 +32,30 @@ resource "aws_ecs_task_definition" "botletics" {
   ])
 }
 
+resource "aws_ecs_task_definition" "botletics_migrator" {
+  family                   = "botletics-migrator"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["EC2"]
+  container_definitions = jsonencode([
+    {
+      name      = "botletics-migrator-1"
+      image     = "${aws_ecr_repository.botletics_migrator.repository_url}:latest"
+      essential = true
+      memory    = 128
+      environment = [
+        {
+          name  = "DATABASE_URL"
+          value = local.postgres_connection_url
+        }
+      ]
+    }
+  ])
+}
+
 resource "aws_ecs_service" "botletics" {
-  name            = "botletics"
+  name            = "botletics-server"
   cluster         = aws_ecs_cluster.botletics.id
-  task_definition = aws_ecs_task_definition.botletics.arn
+  task_definition = aws_ecs_task_definition.botletics_server.arn
   desired_count   = 1
 
   network_configuration {
