8e619c6b0df06dfc2c094479d2a2a13b2fadbc80
Max Synnott <maxryansynnott@gmail.com>
2021-11-12 16:47:32 +0800

    Changed random chess bot response format

--- bots/chess/random/index.ts
+++ bots/chess/random/index.ts
@@ -5,8 +5,18 @@ interface Event {
 	body: string
 }
 
-interface Response {
+interface ResponseBody {
 	move: string
+}
+
+interface Response {
+	isBase64Encoded: boolean
+	statusCode: number
+	statusDescription: string
+	headers: {
+		'Content-Type': string
+	}
+	body: string
 }
 
 export const handler = async (event: Event): Promise<Response> => {
@@ -19,5 +29,17 @@ export const handler = async (event: Event): Promise<Response> => {
 	// Problem with just-random types. Submitted PR
 	if (!move) throw new Error('No moves found')
 
-	return { move }
+	return generateResponse({ move })
+}
+
+const defaultResponse: Response = {
+	isBase64Encoded: false,
+	statusCode: 200,
+	statusDescription: '200 OK',
+	headers: { 'Content-Type': 'application/json' },
+	body: '',
+}
+
+const generateResponse = (body: ResponseBody) => {
+	return { ...defaultResponse, body: JSON.stringify(body) }
 }
