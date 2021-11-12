cbd316feb8dd0946cf3742493f61a27bb5e7062c
Max Synnott <maxryansynnott@gmail.com>
2021-11-12 16:22:21 +0800

    Added logging to random chess bot

--- bots/chess/random/index.ts
+++ bots/chess/random/index.ts
@@ -11,13 +11,17 @@ interface Response {
 	move: string
 }
 
-export const handler = async ({
-	payload: { fen },
-}: Event): Promise<Response> => {
+export const handler = async (event: Event): Promise<Response> => {
+	console.log({ event })
+	const {
+		payload: { fen },
+	} = event
 	const chess = new Chess(fen)
 	const possibleMoves = chess.moves()
+	const move = random(possibleMoves)
 	// TODO: Fix once PR is merged
 	// Problem with just-random types. Submitted PR
-	const move = random(possibleMoves) as unknown as string
+	if (!move) throw new Error('No moves found')
+
 	return { move }
 }
