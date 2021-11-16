033d275f58adb40f82290915d3b1f6b23086c9bc
Max Synnott <maxryansynnott@gmail.com>
2021-11-16 12:15:44 +0800

    Added seeding script

---
+++ servers/server/seed.ts
@@ -0,0 +1,33 @@
+import { db } from './src/clients/db'
+import { hashPassword } from './src/helpers/hashPassword'
+
+const stockfishBotAttributes = (elo: number) => ({
+	name: 'Stockfish ' + elo,
+	endpoint: 'http://localhost:10001/' + elo,
+	type: 'chess',
+})
+
+const userAttributes = [
+	{
+		email: 'user1@gmail.com',
+		passwordHash: hashPassword('123456'),
+		bots: { create: stockfishBotAttributes(1000) },
+	},
+	{
+		email: 'user2@gmail.com',
+		passwordHash: hashPassword('123456'),
+		bots: { create: stockfishBotAttributes(1000) },
+	},
+]
+
+const seed = async () => {
+	await Promise.all(
+		userAttributes.map((attributes) =>
+			db.user.create({ data: attributes }),
+		),
+	)
+}
+
+seed()
+	.then(() => console.log('Successfully seeded'))
+	.catch((e) => console.error(e))
